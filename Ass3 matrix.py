e29fcc9df06aeb94d7a39165d36d781dae4e74ef
ecpracticles <136753085+ecpracticles@users.noreply.github.com>
2023-12-12 21:58:27 +0530

    Add files via upload

--- Ass3 matrix.py
+++ Ass3 matrix.py
@@ -1,44 +1,71 @@
-A=[]
 
-def accept_array(A):
-    n=int(input("Enter the number of students: "))
+M1=[]
+M2=[]
+def matrix(M1):  
+    global m
+    global n
+    m=int(input("Enter the number of rows in 1st Matrix"))
+    n=int(input("Enter the number of columns in 1st Matrix"))
+    print("Enter the Elements for 1st matrix")
+    for i in range (m):
+        A=[]
+        for j in range (n):
+            A.append(int(input()))
+        M1.append(A)
+    print("1st Matrix Entered Succeessfully\n",M1)
+matrix(M1)
+
+def matrix(M2):    
+    m=int(input("Enter the number of rows in 2nd Matrix"))
+    n=int(input("Enter the number of columns in 2nd Matrix"))
+    print("Enter the Elements for 2nd matrix")
+    for i in range (m):
+        B=[]
+        for j in range (n):
+            B.append(int(input()))
+        M2.append(B)
+    print("2nd Matrix Entered Succeessfully\n",M2)
+matrix(M2)
+
+M3=[]
+def add_matrices(M1,M2,M3):
+    for i in range (len(M1)):
+        C=[]
+        for j in range (len(M2)):
+            C.append(M1[i][j]+M2[i][j])
+        M3.append(C)
+    print("Addition Of Matrices is\n",M3)
+add_matrices(M1,M2,M3)
+
+M4=[]
+def sub_matrices(M1,M2,M4):
+    for i in range (len(M1)):
+        D=[]
+        for j in range (len(M2)):
+            D.append(M1[i][j]-M2[i][j])
+        M4.append(D)
+    print("Subtraction Of Matrices is\n",M4)
+sub_matrices(M1,M2,M4)
+
+M5=[]
+def mul_matrices(M1,M2,M5):
+    for i in range (len(M1)):
+        E=[]
+        for j in range (len(M2)):
+            sum=0
+            for k in range (n):
+                sum=sum+(M1[i][k]*M2[k][j])
+            E.append(sum)
+        M5.append(E)
+    print("Multiplication of Matrices is\n",M5)
+mul_matrices(M1,M2,M5)
+
+M6=[]
+def transpose(M1,M6):
     for i in range (n):
-        x=float(input("Enter FE percentage of student: "))
-        A.append(x)
-    print("Data entered succesfully!\n",A)
-accept_array(A)
-
-def selection_sort(A):
-    print("selection")
-    n = len(A)
-    for j in range(n-1):
-        min=j
-        for i in range (j+1,n):
-            if(A[i] < A[min]):
-                min=i
-        temp=A[j]
-        A[j]=A[min]
-        A[min]=temp
-    print("Here is your Sorted array\n",A)
-selection_sort(A)
-
-def bubble_sort(A):
-    print("bubble")
-    n = len(A)
-    for k in range(1,n):
-        for i in range(n-k):
-            if(A[i] < A[i+1]):
-                temp=A[i]
-                A[i]=A[i+1]
-                A[i+1]=temp
-    print("Here is your Sorted array\n",A)
-
-    if(len(A)>=5):
-        print("Top 5 scores: ")
-        for i in range (5):
-            print(A[i])
-    else :
-       print("Top Scorers : ")
-       for i in range(len(A)) :
-          print(A[i])                  
-bubble_sort(A)
+        F=[]
+        for j in range (m):
+            F.append(M1[j][i])
+        M6.append(F)
+    print("Transpose of 1st Matrix is\n",M6)
+transpose(M1,M6)
